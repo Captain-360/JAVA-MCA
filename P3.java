f4c6c32b67dd88428fa178bdb412de3c7a4270f4
Captain-360 <captainspeaking360@gmail.com>
2026-09-16 13:42:51 +0530

    Add files via upload

---
+++ P3.java
@@ -0,0 +1,17 @@
+// Write a Java program for a University Management System where the Student and
+// Faculty classes inherit common attributes and methods from a parent Person class.
+// Demonstrate code reusability through inheritance.
+
+class Student{
+    String name;
+    long mono;
+    String email;
+    int age;
+    String gender;
+    
+}
+public class P3 {
+    public static void main(String[] args) {
+        
+    }
+}
